eaaf8a724305a1051d441ad4a199b712359f536c
Rufus Pollock <rufus.pollock@okfn.org>
2013-05-02 08:49:25 +0100

    [scrape.js][s]: serialize to csv rather than json.

--- scrape.js
+++ scrape.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var fs = require('fs');
 var cheerio = require('cheerio');
+var csv = require('csv');
 
 var ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17';
 var BAILII = 'http://www.bailii.org/ew/cases/';
@@ -18,50 +19,62 @@ function scrapePage() {
 // index pages look like ...
 // /ew/cases/EWHC/Comm/
 // /ew/cases/EWHC/Comm/1995/
+
+// DB structure
+var headers = [ 'id', 'court', 'division', 'year', 'number', 'date', 'url', 'title' ];
+// Court = Court (Group) Code (e.g. EWHCi = England and Wales High Court)
+// number = bailii number
 function buildIndex() {
   // by courts then years then cases ...
-  var index = {
-    // england-wales high court - commercial
-    'EWHC/Comm': {}
-  };
+  var index = [];
   var court = 'EWHC/Comm';
-  // for comm court starts at 1995 and runs to present ...
-  for (x=1995;x<=2012;x++) {
-    index[court][x] = {};
-  }
 
-  for (year in index[court]) {
+  // for comm court starts at 1995 and runs to present ...
+  for (year=1995;year<=2012;year++) {
     var url = BAILII + court + '/' + year + '/';
-    getInfo(url, court, year);
+    getInfo(url, court, year, function() {
+      save(index);
+    });
   }
 
   // split out to avoid scoping issues
-  function getInfo(url, court, year) {
+  function getInfo(url, court, year, cb) {
     request(url, function(error, response, body) {
       console.log('Processing court ' + court + ' for year: ' + year);
       $ = cheerio.load(body);
       // <li><a href="/ew/cases/EWHC/Comm/2012/87.html">Abuja International Hotels Ltd. v Meridien Sas <a title="Link to BAILII version" href="/ew/cases/EWHC/Comm/2012/87.html">[2012] EWHC 87 (Comm)</a> (26 January 2012)</a></li>
       $('li').each(function(idx, html) {
-        var data = {};
+        var data = {
+          year: year,
+          court: court.split('/')[0],
+          division: court.split('/')[1]
+        };
         var $a = $(html).find('a').first();
         data.url = $a.attr('href');
         // note numbers are not always actual integers e.g. B26
-        data.bailii_number = data.url.split('/').reverse()[0].split('.html')[0];
+        data.number = data.url.split('/').reverse()[0].split('.html')[0];
         var _content = $a.html();
         var _parts = _content.split(' <a');
         data.title = _parts[0];
-        data.rawdate = _parts[1].split('</a> (').reverse()[0].replace(')', '');
-        data.date = new Date(data.rawdate).toISOString().slice(0, 10);
-        data.id = [court, year, data.bailii_number].join('::');
-        index[court][year][data.bailii_number] = data;
-        save();
+        var rawdate = _parts[1].split('</a> (').reverse()[0].replace(')', '').replace(',', '').replace('th', '').replace('rd', '').replace('st', '');
+        try {
+          data.date = new Date(rawdate).toISOString().slice(0, 10);
+        } catch(e) {
+          console.log(_content);
+          console.log(rawdate);
+        }
+        data.id = [data.court, data.division, data.year, data.number].join('-');
+        index.push(data);
+        cb();
       });
     });
   }
 
-  function save() {
-    var data = JSON.stringify(index, null, 2);
-    fs.writeFileSync('cache/scrape/index.json', data);
+  function save(data) {
+    csv()
+      .from(data)
+      .to.path('data/cases.csv', {columns: headers, header: true} )
+      ;
   }
 }
 
